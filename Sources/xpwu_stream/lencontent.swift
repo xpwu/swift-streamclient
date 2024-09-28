fbb54b4ab300d7349869f477d210688e2b47eb3b
xpwu <diodexp@foxmail.com>
2024-09-28 14:47:40 +0800

    update code

---
+++ Sources/xpwu_stream/lencontent.swift
@@ -0,0 +1,300 @@
+//
+//  File.swift
+//  
+//
+//  Created by xpwu on 2024/9/26.
+//
+
+import Foundation
+import xpwu_x
+import xpwu_concurrency
+
+/*
+lencontent protocol:
+
+ 1, handshake protocol:
+
+									 client ------------------ server
+											|                          |
+											|                          |
+									 ABCDEF (A^...^F = 0xff) --->  check(A^...^F == 0xff) --- N--> over
+										(A is version)
+											|                          |
+											|                          |Y
+											|                          |
+ version 1:   set client heartbeat  <----- HeartBeat_s (2 bytes, net order)
+ version 2:       set config     <-----  HeartBeat_s | FrameTimeout_s | MaxConcurrent | MaxBytes | connect id
+																					HeartBeat_s: 2 bytes, net order
+																					FrameTimeout_s: 1 byte
+																					MaxConcurrent: 1 byte
+																					MaxBytes: 4 bytes, net order
+																					connect id: 8 bytes, net order
+											|                          |
+											|                          |
+											|                          |
+											data      <-------->       data
+
+
+	 2, data protocol:
+		 1) length | content
+			 length: 4 bytes, net order; length=sizeof(content)+4; length=0 => heartbeat
+*/
+
+private actor ClosedBySelf {
+	var bySelf: Bool = false
+	
+	var isTrue: Bool {
+		get {bySelf}
+	}
+	
+	func yes() {
+		bySelf = true
+	}
+}
+
+public class LenContent {
+	fileprivate var config: Option.Value = Option.Value()
+	private var handshake: Handshake = Handshake()
+	private var task: URLSessionStreamTask?
+	private let mutex: Mutex = Mutex()
+	private var closedBySelf = ClosedBySelf()
+	private let heartbeatStop: Channel<Bool> = Channel(buffer: .Unlimited)
+	
+	public var logger: Logger = PrintLogger()
+	public var onMessage: ([Byte])async -> Void = { _ in}
+	public var onError: (StmError)async -> Void = { _ in}
+	
+	public convenience init(_ options: Option...) {
+		self.init(options)
+	}
+	
+	public init(_ options: [Option]) {
+		for option in options {
+			option.runner(&config)
+		}
+	}
+	
+	deinit {
+		task?.cancel()
+	}
+}
+
+public extension LenContent {
+	struct Option {
+		fileprivate struct Value {
+			var host: String = "127.0.0.1"
+			var port: Int = 8080
+			var connectionTimeout: Duration = 30*Duration.Second
+		}
+		fileprivate let runner: (inout Value)->Void
+		
+		fileprivate init(_ runner: @escaping (inout Value) -> Void) {
+			self.runner = runner
+		}
+		
+		public static func Host(host: String)-> Option {
+			return Option { value in
+				value.host = host
+			}
+		}
+		public static func Port(port: Int)-> Option {
+			return Option { value in
+				value.port = port
+			}
+		}
+		public static func ConnectTimeout(t: Duration)-> Option {
+			return Option { value in
+				value.connectionTimeout = t
+			}
+		}
+	}
+}
+
+fileprivate class SessionDelegate: NSObject, URLSessionStreamDelegate {
+	func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: (any Error)?) {
+		if let error {
+			self.runner(task as! URLSessionStreamTask, .ElseConnErr("\(error)"))
+		} else {
+			self.runner(task as! URLSessionStreamTask, nil)
+		}
+	}
+	
+	fileprivate let runner:(URLSessionStreamTask, StmError?)->Void
+	init(_ r: @escaping (URLSessionStreamTask, StmError?)->Void) {
+		self.runner = r
+	}
+}
+
+func handshakeReq() -> Data {
+	var handshake = Data(repeating: 0, count: 6)
+	// version: 2
+	handshake[0] = 2
+	handshake[1] = Byte(Int.random(in: 0..<256))
+	handshake[2] = Byte(Int.random(in: 0..<256))
+	handshake[3] = Byte(Int.random(in: 0..<256))
+	handshake[4] = Byte(Int.random(in: 0..<256))
+	handshake[5] = 0xff
+	for i in 0..<5 {
+		handshake[5] ^= handshake[i]
+	}
+	
+	return handshake
+}
+
+extension LenContent {
+	func read() {
+		Task {
+			while task?.state == .running {
+				do {
+					var (data, eof) = try await task!.readData(ofMinLength: 1, maxLength: 4
+																	 , timeout:(2*self.handshake.HearBeatTime).timeInterval())
+					if data == nil  || eof {
+						throw StmError.ElseConnErr("read length error, maybe connection closed by peer")
+					}
+					
+					let len: UInt32 = data!.toBytes().net2UInt32()
+					// heartbeat
+					if len == 0 {
+						continue
+					}
+					
+					// 出现这种情况，很可能是协议出现问题了，而不能单纯的认为是本次请求的问题
+					if len > self.handshake.MaxBytes {
+						throw StmError.ElseConnErr("received Too Large data(len=\(len)), must be less than \(self.handshake.MaxBytes)")
+					}
+					
+					var res: [Byte] = []
+					while res.count < len {
+						(data, eof) = try await task!.readData(ofMinLength: 1, maxLength: Int(len)
+																		 , timeout:self.handshake.FrameTimeout.timeInterval())
+						if data == nil  || eof {
+							throw StmError.ElseConnErr("read content error, maybe connection closed by peer")
+						}
+						res.append(contentsOf: data!)
+					}
+					
+					await onMessage(res)
+				} catch StmError.ElseConnErr(let msg) {
+					if !(await closedBySelf.isTrue) {
+						await onError(StmError.ElseConnErr(msg))
+					}
+					break
+				} catch {
+					if !(await closedBySelf.isTrue) {
+						await onError(StmError.ElseConnErr("\(error)"))
+					}
+					break
+				}
+			}
+		}
+	}
+}
+
+extension LenContent: `Protocol` {
+	public func Connect() async -> (Handshake, StmError?) {
+		let (task, err) = await withCheckedContinuation {
+			(continuation: CheckedContinuation<(URLSessionStreamTask, StmError?), Never>) in
+			
+			var c = URLSessionConfiguration.default
+			c.timeoutIntervalForRequest = TimeInterval(config.connectionTimeout.second())
+			
+			let task = URLSession(configuration: c, delegate: SessionDelegate({
+				(task, err) -> Void in
+				continuation.resume(returning: (task, err))
+			}), delegateQueue: nil).streamTask(withHostName: config.host, port: config.port)
+			
+			task.resume()
+		}
+		
+		if let err {
+			return (Handshake(), err)
+		}
+		
+		do {
+			try await task.write(handshakeReq(), timeout: TimeInterval(config.connectionTimeout.second()))
+			let (h, eof) = try await task.readData(ofMinLength: Handshake.StreamLen, maxLength: Handshake.StreamLen
+													, timeout: TimeInterval(config.connectionTimeout.second()))
+			if eof {
+				throw StmError.ElseConnErr("connected eof")
+			}
+			
+			guard let h else {
+				throw StmError.ElseConnErr("no handshake response")
+			}
+			
+			self.handshake = Handshake.Parse(h.toBytes())
+			self.setOutputHeartbeat()
+			self.read()
+			
+		}catch  {
+			task.cancel()
+			return (Handshake(), StmError.ElseConnErr("\(error)"))
+		}
+
+		self.task = task
+		return (self.handshake, nil)
+	}
+	
+	public func Close() async {
+		task?.cancel()
+	}
+	
+	public func Send(content: [Byte]) async -> StmError? {
+		// sizeof(length) = 4
+		if content.count + 4 > self.handshake.MaxBytes {
+			return StmError.ElseErr("request.size(\(content.count)) > MaxBytes(\(self.handshake.MaxBytes-4))")
+		}
+		
+		var len:[Byte] = [0, 0, 0, 0]
+		UInt32(content.count + 4).toNet(&len)
+		
+		await stopOutputHeartbeat()
+		await mutex.WithLock {
+			do {
+				try await task?.write(len.toData(), timeout: TimeInterval(self.handshake.FrameTimeout.second()))
+				try await task?.write(content.toData(), timeout: TimeInterval(self.handshake.FrameTimeout.second()))
+			} catch {
+				await onError(.ElseConnErr("\(error)"))
+			}
+		}
+		
+		setOutputHeartbeat()
+		// todo return connect error?
+		return nil
+	}
+}
+
+// output heartbeat
+extension LenContent {
+	func setOutputHeartbeat() {
+		Task {
+			let timeout = await WithTimeout(self.handshake.HearBeatTime) {
+				await self.heartbeatStop.Receive()
+			}
+			
+			// not timeout: stopped
+			if let timeout {
+				return
+			}
+			
+			let ok = await mutex.WithLock {
+				do {
+					try await self.task?.write(Data(repeating: 0, count: 4)
+																		 , timeout: self.handshake.FrameTimeout.timeInterval())
+					return true
+				} catch {
+					await self.onError(.ElseErr("\(error)"))
+					return false
+				}
+			}
+			
+			if ok  {
+				setOutputHeartbeat()
+			}
+		}
+	}
+	
+	func stopOutputHeartbeat()async {
+		await self.heartbeatStop.Send(true)
+	}
+}
--- Sources/xpwu_stream/net.swift
+++ Sources/xpwu_stream/net.swift
@@ -7,14 +7,13 @@
 
 import Foundation
 import xpwu_concurrency
-
-private let reqIdStart: UInt64 = 10
+import xpwu_x
 
 private typealias RequestChannel = Channel<(FakeHttp.Response, StmError?)>
 
 fileprivate class SyncAllRequest {
 	let mutex: Mutex = Mutex()
-	var allRequests: [UInt64:RequestChannel] = [:]
+	var allRequests: [UInt32:RequestChannel] = [:]
 	var semaphore: xpwu_concurrency.Semaphore = Semaphore(permits: 3)
 	
 	public var permits: Int {
@@ -28,7 +27,7 @@ fileprivate class SyncAllRequest {
 }
 
 extension SyncAllRequest {
-	func Add(reqId: UInt64) async -> RequestChannel {
+	func Add(reqId: UInt32) async -> RequestChannel {
 		await semaphore.Acquire()
 		return await mutex.WithLock {
 			let ch = RequestChannel(buffer: 1)
@@ -38,7 +37,7 @@ extension SyncAllRequest {
 	}
 	
 	// 可以用同一个 reqid 重复调用
-	func Remove(reqId: UInt64) async -> RequestChannel? {
+	func Remove(reqId: UInt32) async -> RequestChannel? {
 		return await mutex.WithLock {
 			let ret = allRequests.removeValue(forKey: reqId)
 			// todo: check semaphore
@@ -63,7 +62,7 @@ extension SyncAllRequest {
 
 /**
  *
- *    NotConnect  --->  Connecting  ---> Connected ---> Invalidated
+ *    NotConnect  ---> (Connecting)  ---> Connected ---> Invalidated
  *                          |                                ^
  *                          |                                |
  *                          |________________________________|
@@ -87,6 +86,239 @@ extension State: CustomStringConvertible {
 	}
 }
 
-class Net {
+extension State: Equatable {
+	static func == (lhs: State, rhs: State) -> Bool {
+		switch lhs {
+		case .NotConnect:
+			switch rhs {
+			case .NotConnect:
+				return true
+			default :
+				return false
+			}
+		case .Connected:
+			switch rhs {
+			case .Connected:
+				return true
+			default:
+				return false
+			}
+		case .Invalidated(let stmError):
+			switch rhs {
+			case .Invalidated(let stmError):
+				return true
+			default:
+				return false
+			}
+		}
+	}
 	
+	var isInvalidated: Bool {
+		get { self == .Invalidated(StmError.ElseErr(""))}
+	}
+}
+
+actor ReqId {
+	private static let reqIdStart: UInt32 = 10
+	
+	var value: UInt32 = reqIdStart
+	
+	func get()-> UInt32 {
+		value += 1
+		if value < ReqId.reqIdStart || value > UInt32.max {
+			value = ReqId.reqIdStart
+		}
+		
+		return value
+	}
+}
+
+class Net {
+	private let logger: Logger
+	private let onPeerClosed: (StmError) async -> Void
+	private let onPush: ([Byte]) async -> Void
+	
+	var isInValid: Bool {
+		get { state.isInvalidated }
+	}
+
+	private var handshake: Handshake = Handshake()
+
+	private let connLocker: Mutex = Mutex()
+	private var state: State = State.NotConnect
+	private var proto: `Protocol`
+
+	private var reqId: ReqId = ReqId()
+	private var allRequests: SyncAllRequest = SyncAllRequest()
+
+	private let flag = UniqFlag()
+	
+	var connectID: String {
+		get {handshake.ConnectId}
+	}
+
+	init(_ l: Logger, protocolCreator:()->`Protocol`
+			 , onPeerClosed: @escaping(StmError)async->Void, onPush: @escaping([Byte])async->Void) {
+		self.logger = l
+		self.onPush = onPush
+		self.onPeerClosed = onPeerClosed
+		
+		self.proto = protocolCreator()
+		self.proto.logger = l
+		self.proto.onError = {[unowned self](err)async->Void in await self.onError(err)}
+		self.proto.onMessage = {[unowned self](msg)async->Void in await self.onMessage(msg)}
+		
+		logger.Debug("Net[$flag].new", "flag=$flag, protocol.hashcode=$ph")
+	}
+}
+
+private extension Net {
+	func closeAndOldState(err: StmError) async -> State {
+		let old = await connLocker.WithLock {
+			let old = self.state
+			
+			if self.state.isInvalidated {
+				return old
+			}
+			self.state = .Invalidated(err)
+			
+			return old
+		}
+		
+		await allRequests.ClearAllWith(ret: (FakeHttp.Response(), err.toConnError))
+		
+		return old
+	}
+}
+
+extension Net {
+	// 可重复调用
+	func connect()async -> StmError? {
+		return await connLocker.WithLock {
+			if state == .Connected {
+				return nil
+			}
+			if case State.Invalidated(let err) = state {
+				return err
+			}
+			
+			// state.NotConnect
+			let(handshake, err) = await self.proto.Connect()
+			if let e = err {
+				self.state = .Invalidated(e)
+				return e
+			}
+			
+			// OK
+			self.state = .Connected
+			self.handshake = handshake
+			self.allRequests.permits = self.handshake.MaxConcurrent
+			
+			return nil
+		}
+	}
+	
+	// 如果没有连接成功，直接返回失败
+	func send(data: [Byte], headers:[String:String]
+						, timeout:Duration = 30*Duration.Second) async -> ([Byte], StmError?) {
+		// 预判断
+		let ret = await connLocker.WithLock { ()->StmError? in
+			if case State.Invalidated(let err) = state {
+				return err.toConnError
+			}
+			if state != .Connected {
+				return .ElseConnErr("not connected")
+			}
+			return nil
+		}
+		if let ret {
+			return ([], ret)
+		}
+		
+		let reqId = await self.reqId.get()
+		let (request, err) = FakeHttp.Request.New(reqId: reqId, body: data, headers: headers)
+		if let err {
+			return ([], err)
+		}
+		if request.encodedData.count > self.handshake.MaxBytes {
+			return ([], .ElseErr("request.size(\(request.encodedData.count)) > MaxBytes(\(handshake.MaxBytes))"))
+		}
+		
+		// 在客户端超时也认为是一个请求结束，但是真正的请求并没有结束，所以在服务器看来，仍然占用服务器的一个并发数
+		// 因为网络异步的原因，客户端并发数不可能与服务器完全一样，所以这里主要是协助服务器做预控流，按照客户端的逻辑处理即可
+		
+		let ch = await allRequests.Add(reqId: reqId)
+		let ret2 = await WithTimeout(timeout) {
+			Task {
+				let err = await self.proto.Send(content:request.encodedData)
+				if let err {
+					// todo  ch?
+					await ch.Send((FakeHttp.Response(), err))
+				}
+			}
+			return await ch.Receive()
+		}
+		// timeout: ret2 == nil
+		guard let ret2 else {
+			return ([], .ElseTimeoutErr("request timeout(\(timeout.second())s)"))
+		}
+		
+		if let err = ret2.1 {
+			return ([], err)
+		}
+		
+		if ret2.0.status != .OK {
+			return ([],.ElseErr(String(decoding: ret2.0.data, as: UTF8.self)))
+		}
+		
+		_ = await allRequests.Remove(reqId: reqId)
+		
+		return (ret2.0.data, nil)
+	}
+	
+	func close()async {
+		let oldState = await closeAndOldState(err: .ElseErr("closed by self"))
+		if oldState == .Connected {
+			await self.proto.Close()
+		}
+	}
+}
+
+// delegate
+extension Net {
+	func onMessage(_ msg: [Byte]) async {
+		let (response, err) = msg.Parse()
+		if let err {
+			await onError(err)
+			return
+		}
+		
+		if response.isPush {
+			let (pushAck, err) = response.newPushAck()
+			if let err {
+				await onError(err)
+				return
+			}
+			
+			async let _ = self.onPush(response.data)
+			// ignore error
+			async let _ = self.proto.Send(content: pushAck)
+			return
+		}
+		
+		let ch = await allRequests.Remove(reqId: response.reqId)
+		guard let ch else {
+			return
+		}
+		
+		async let _ = ch.Send((response, nil))
+	}
+	
+	func onError(_ err: StmError) async {
+		let oldState = await closeAndOldState(err: err)
+		if oldState == .Connected {
+			async let _ = self.onPeerClosed(err)
+			await self.proto.Close()
+		}
+	}
 }
