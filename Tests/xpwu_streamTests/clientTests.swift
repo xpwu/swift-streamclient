29bc5e42b030af950b051af5f65436a0b813d1d5
xpwu <diodexp@foxmail.com>
2024-09-29 15:41:55 +0800

    Update clientTests.swift

--- Tests/xpwu_streamTests/clientTests.swift
+++ Tests/xpwu_streamTests/clientTests.swift
@@ -10,7 +10,10 @@ final class streamclientTests: XCTestCase {
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
 	
-	func testError() {
+	func testError()async {
 		print(StmError.ConnTimeoutErr("timeout > 5s"))
+		print({()->String in "test auto called closure1"}())
+		async let a = {()async->String in "test auto called closure2"}()
+		print(await a)
 	}
 }
